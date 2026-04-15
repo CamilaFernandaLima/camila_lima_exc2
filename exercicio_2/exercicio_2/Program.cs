e2717f286c60de1cda86661000414b255d25958b
Camila Fernanda e Silva Lima <camila.fernanda@unifesp.br>
2026-04-15 14:58:11 -0300

    implementação da classe HomeTheater.

--- exercicio_2/exercicio_2/Program.cs
+++ exercicio_2/exercicio_2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Versioning;
 
 // 1. criação dos objetos do sistema home theater
 public class TV
@@ -78,4 +79,68 @@ public class LuzAmbiente
     {
         Console.WriteLine("Luzes: Acessas em intensidade normal.");
     }
+    public void Desligar()
+    {
+        Console.WriteLine("Luzes: Desligadas.");
+    }
 }
+
+
+// 2. criação da fachada (Facade) 
+public class HomeTheaterFacade
+{
+    //guarda referencias aos subsistemas:
+    private TV tv;
+    private Projetor projetor;
+    private Receiver receiver;
+    private PlayerMidia player;
+    private SistemaSom som;
+    private LuzAmbiente luz;
+
+    public HomeTheaterFacade()
+    {
+        tv = new TV();
+        projetor = new Projetor();
+        receiver = new Receiver();
+        player = new PlayerMidia();
+        som = new SistemaSom();
+        luz = new LuzAmbiente();
+    }
+
+    //operação AssistirFilme()
+    public void AssistirFilme()
+    {
+        Console.WriteLine("Preparando ambiente para assistir filme.");
+        luz.Escurecer();
+        projetor.Ligar();
+        receiver.Ligar();
+        receiver.ModoSurround();
+        som.Ligar();
+        player.Ligar();
+        player.Reproduzir();
+    }
+
+    //operação OuvirMusica()
+    public void OuvirMusica()
+    {
+        Console.WriteLine("Preparando ambiente para ouvir música");
+        luz.Ligar();
+        tv.Ligar();
+        receiver.Ligar();
+        receiver.ModoStereo();
+        player.Ligar();
+        player.Reproduzir();
+    }
+
+    //desligamento
+    public void DesligarTudo()
+    {
+        Console.WriteLine("Desligando todos os dispositivos.");
+        luz.Desligar();
+        tv.Desligar();
+        projetor.Desligar();
+        receiver.Desligar();
+        som.Desligar();
+        player.Desligar();
+    }
+} 
